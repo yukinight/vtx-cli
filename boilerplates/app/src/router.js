7d38d6cac580be4e070b89215e86c1a0ddf06279
TianQi Zhang <jsxsztq@163.com>
2018-02-27 17:20:25 +0800

    2.0  init

--- boilerplates/app/src/router.js
+++ boilerplates/app/src/router.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Router, Route } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import { Router, Route, Switch } from 'dva/router';
+import dynamic from 'dva/dynamic';
 
-function RouterConfig({ history }) {
-  return (
-    <Router history={history}>
-      <Route path="/" component={IndexPage} />
-    </Router>
-  );
+const routes = [{
+    path: '/',
+    models: () => [import('./models/example')],
+    component: () => import('./routes/IndexPage'),
+}];
+
+function RouterConfig({ history, app }) {
+    return (
+        <Router history={history}>
+        <Switch>
+            {
+                routes.map(({ path, ...dynamics }, key) => (
+                    <Route
+                        key={key}
+                        exact
+                        path={path}
+                        component={dynamic({
+                        app,
+                        ...dynamics,
+                        })}
+                    />
+                ))
+            }
+        </Switch>
+        </Router>
+    );
 }
 
 export default RouterConfig;
