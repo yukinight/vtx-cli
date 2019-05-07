c4fd5fbf52105c7f8ba2ac99155a7bdddf845b85
TianQi Zhang <jsxsztq@163.com>
2019-05-07 17:43:35 +0800

    优化打包工具的打包速度，初次打包速度可提升80%，二次打包速度可提高90%

--- boilerplates/app/webpack.config.js
+++ boilerplates/app/webpack.config.js
@@ -1,16 +1,35 @@
 var webpack = require('webpack');
+var path = require('path');
 var CSSSplitWebpackPlugin = require('css-split-webpack-plugin/dist/index').default;
-var historyDir = require('path').dirname(require.resolve('history/package.json'),);
+var ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 
 module.exports = function(webpackConfig, env) {
 // adding plugins to your configuration
     webpackConfig.plugins.push( 
         new CSSSplitWebpackPlugin({
-            size: 3000,
+            size: 3000
         })
     )
-    // 锁定版本
-    webpackConfig.resolve.alias['history'] = historyDir;
+    webpackConfig.plugins = webpackConfig.plugins.filter(function(plugin) {
+        return !(plugin instanceof webpack.optimize.UglifyJsPlugin);
+    });
+    webpackConfig.plugins.push(new ParallelUglifyPlugin({
+        uglifyJS: { 
+            output: {
+                comments: false,
+                ascii_only: true
+            },
+            warnings: false,
+        },
+        cacheDir: './.cache'
+      }));
+
+    // 指定包路径
+    function setAlias(pkgName){
+        webpackConfig.resolve.alias[pkgName] = path.dirname(require.resolve(`${pkgName}/package.json`));        
+    }
+    setAlias('history');
+    setAlias('moment');
 
     return webpackConfig
 }
